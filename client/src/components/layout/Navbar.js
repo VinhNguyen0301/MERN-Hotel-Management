52d27b753569fd28e3907a9fed41e2838e1bc280
Vinh Nguyen <vinh.nguyen@codestringers.com>
2021-03-22 23:47:32 +0700

    Change format code

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,24 +1,57 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.scss";
+import Card from "react-bootstrap/Card";
+
 
 class Navbar extends Component {
   render() {
     return (
-      <div className="navbar-fixed">
-        <nav className="z-depth-0">
-          <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">code</i>
-              MERN
+      <div className=" navbar-background">
+
+        <div className=" nav-style">
+          <Link
+            to="/"
+          >
+            MERN AIR-BNB
             </Link>
+        </div>
+
+        <div className="btn-position">
+          <Link
+            to="/register"
+            style={{
+              width: "140px",
+              borderRadius: "3px",
+              letterSpacing: "1.5px"
+            }}
+            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+          >
+            Register
+              </Link>
+          <Link
+            to="/login"
+            style={{
+              width: "140px",
+              borderRadius: "3px",
+              letterSpacing: "1.5px"
+            }}
+            className="btn btn-large btn-flat waves-effect white black-text"
+          >
+            Log In
+              </Link>
+        </div>
+        <article className="room">
+          <div className="img-container">
+            <img alt="single room" />
+            <div className="price-top">
+              <h6></h6>
+              <p>per night</p>
+            </div>
+
           </div>
-        </nav>
+          <p className="room-info"></p>
+        </article>
       </div>
     );
   }
